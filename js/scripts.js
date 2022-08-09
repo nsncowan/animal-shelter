1ab38951a800ffd74dcd04ec10eda3bc727e7394
nsncowan <nsncowan@gmail.com>
2022-08-09 16:00:47 -0700

    add initial javascript to animal shelter page.
    co-authored-by : Auston Moos
    <austonhmoos2004@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+// on load
+
+// when 'dark mode is clicked...
+
+
+
+//'dark mode': function to set background to dark, and text to light
+function darkmode() {
+  document.body.style.backgroundcolor = "black"
+};
+
+
+//'light mode' : function to set background to light, and text to dark
+function lightmode() {
+  document.body.style.bacgroundcolor = "white"
+};
+
+//large font function
+
+
+//regular font function
